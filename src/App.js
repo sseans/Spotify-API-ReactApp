579e686069ce665218c2a3cedc6801b4f0326f74
sseans <41116820+sseans@users.noreply.github.com>
2021-05-31 14:34:40 +1000

    styled components running

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledFakeContent = styled.header`
+  width: 90%;
+  height: 2000px;
+  background-color: var(--slate);
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <StyledApp>
+      <StyledFakeContent>Hello</StyledFakeContent>
+    </StyledApp>
+  );
 }
 
 export default App;
---
+++ src/Styles/GlobalStyle.js
@@ -0,0 +1,78 @@
+import { createGlobalStyle } from "styled-components";
+import variables from "./Variables";
+
+const GlobalStyle = createGlobalStyle`
+  ${variables};
+  html {
+    box-sizing: border-box;
+    width: 100%;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  ::selection {
+    background-color: var(--lightest-navy);
+    color: var(--lightest-slate);
+  }
+  :focus {
+    outline: 2px dashed var(--green);
+    outline-offset: 3px;
+  }
+  /* Scrollbar Styles */
+  html {
+    scrollbar-width: thin;
+    scrollbar-color: var(--dark-slate) var(--navy);
+  }
+  body::-webkit-scrollbar {
+    width: 12px;
+  }
+  body::-webkit-scrollbar-track {
+    background: var(--navy);
+  }
+  body::-webkit-scrollbar-thumb {
+    background-color: var(--dark-slate);
+    border: 3px solid var(--navy);
+    border-radius: 10px;
+  }
+  body {
+    margin: 0;
+    width: 100%;
+    min-height: 100%;
+    overflow-x: hidden;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    background-color: var(--navy);
+    color: var(--slate);
+    font-family: var(--font-sans);
+    font-size: var(--fz-xl);
+    line-height: 1.3;
+    @media (max-width: 480px) {
+      font-size: var(--fz-lg);
+    }
+    &.hidden {
+      overflow: hidden;
+    }
+    &.blur {
+      overflow: hidden;
+      header {
+        background-color: transparent;
+      }
+      #content > * {
+        filter: blur(5px) brightness(0.7);
+        transition: var(--transition);
+        pointer-events: none;
+        user-select: none;
+      }
+    }
+  }
+  #root {
+    min-height: 100vh;
+    display: grid;
+    grid-template-rows: 1fr auto;
+    grid-template-columns: 100%;
+  }
+`;
+
+export default GlobalStyle;
