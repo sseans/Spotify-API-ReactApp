4b762777a94f29592c0fe2a69eed4925f438552a
sseans <41116820+sseans@users.noreply.github.com>
2021-06-03 11:14:31 +1000

    refresh and login useAuth done

--- client/src/Hooks/useAuth.js
+++ client/src/Hooks/useAuth.js
@@ -8,11 +8,38 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("http:localhost:3001/login", {
+      .post("http://localhost:3001/login", {
         code,
       })
       .then((res) => {
-        console.log("res.data :>> ", res.data);
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        window.location = "/";
       });
   }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location = "/";
+        });
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const cors = require("cors");
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "790b0e732d4f4ac397b1207b4a54b1da",
+    clientSecret: "add6a8f91de0425492e4169db59d58f4",
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
