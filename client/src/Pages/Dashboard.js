47b0afb7a42f6ab37a5e783f249cfa4ad47dc910
sseans <41116820+sseans@users.noreply.github.com>
2021-06-07 16:02:23 +1000

    back on track

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useAuth from "../Hooks/useAuth.js";
+import SpotifyWebApi from "spotify-web-api-node";
+import ClipLoader from "react-spinners/ClipLoader";
+// import styled from "styled-components";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "790b0e732d4f4ac397b1207b4a54b1da",
+});
+
+const tracks = [];
 
 export default function Dashboard({ code }) {
+  let [loading, setLoading] = useState(true);
   const accessToken = useAuth(code);
-  return <div>{code}</div>;
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+    setLoading(false);
+    fillData();
+  }, [accessToken]);
+
+  function fillData() {
+    spotifyApi.getMySavedTracks().then(
+      (data) => {
+        data.body.items.map((x) => {
+          tracks.push(x.track);
+          return x;
+        });
+        console.log(tracks);
+        console.log("Some information about the authenticated user", data);
+        console.log(`name`, data.body.items[0].track.name);
+        console.log(`artist`, data.body.items[0].track.artists[0].name);
+      },
+      (err) => {
+        console.log("Something went wrong!", err);
+      }
+    );
+  }
+
+  console.log("Dashboard Rendered");
+
+  return loading ? (
+    <ClipLoader color="#1ed760" loading={loading} size={150} />
+  ) : (
+    <div>hello</div>
+  );
 }
